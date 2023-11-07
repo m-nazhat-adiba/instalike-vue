e443f68104faf2548afd20e9ddebf21cdf31425e
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-11-07 11:41:52 +0700

    added navbar

--- src/components/common/layout/Layout.vue
+++ src/components/common/layout/Layout.vue
@@ -1,10 +1,12 @@
 <script setup lang="js">
+import NavHeader from '../navigation/NavHeader.vue'
 import NavBar from '../navigation/NavBar.vue'
 </script>
 
 <template>
-  <div class="">
-    <NavBar />
+  <div class="relative">
+    <NavHeader />
     <slot></slot>
+    <NavBar />
   </div>
 </template>
